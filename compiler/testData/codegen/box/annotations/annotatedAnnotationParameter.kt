17f9d718cbd78efe49e06a21426b06164de50bf8
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2022-05-20 14:58:13 +0300

    [NoArg] Add implementation of plugin for FIR

--- compiler/testData/codegen/box/annotations/annotatedAnnotationParameter.kt
+++ compiler/testData/codegen/box/annotations/annotatedAnnotationParameter.kt
@@ -1,18 +1,10 @@
 // TARGET_BACKEND: JVM
 // WITH_STDLIB
+// FULL_JDK
 
-import kotlin.test.assertEquals
-
-annotation class Name(val value: String)
-
-annotation class Anno(
-    @get:Name("O") val o: String,
-    @get:Name("K") val k: String
-)
+import java.util.concurrent.Executors
 
 fun box(): String {
-    val ms = Anno::class.java.declaredMethods
-
-    return (ms.single { it.name == "o" }.annotations.single() as Name).value +
-           (ms.single { it.name == "k" }.annotations.single() as Name).value
+    val pool = Executors.newCachedThreadPool()
+    return "OK"
 }
