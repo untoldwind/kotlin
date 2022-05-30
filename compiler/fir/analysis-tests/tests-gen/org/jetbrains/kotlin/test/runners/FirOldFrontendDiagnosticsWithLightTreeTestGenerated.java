d061594900c3ab38f7751a461ef9961af4044aad
Denis.Zharkov <denis.zharkov@jetbrains.com>
2022-05-30 11:58:32 +0300

    FIR: Fix FP error on a call to constructor of a private inner class
    Do not check dispatch receiver applicability when checking visibility
    for containing class of a callee constructor

--- compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/FirOldFrontendDiagnosticsWithLightTreeTestGenerated.java
+++ compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/FirOldFrontendDiagnosticsWithLightTreeTestGenerated.java
@@ -33421,6 +33421,12 @@ public class FirOldFrontendDiagnosticsWithLightTreeTestGenerated extends Abstrac
             }
 
             @Test
+            @TestMetadata("innerConstructorThroughSubclass.kt")
+            public void testInnerConstructorThroughSubclass() throws Exception {
+                runTest("compiler/testData/diagnostics/tests/visibility/innerConstructorThroughSubclass.kt");
+            }
+
+            @Test
             @TestMetadata("innerNestedAndAnonymousClasses.kt")
             public void testInnerNestedAndAnonymousClasses() throws Exception {
                 runTest("compiler/testData/diagnostics/tests/visibility/innerNestedAndAnonymousClasses.kt");
